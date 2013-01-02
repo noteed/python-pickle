e6d480cdb151dfdfb1f02ace1f47fcdb6a5eb185
Vo Minh Thu <noteed@gmail.com>
2013-01-02 12:26:38 +0100

    Proper command-line, added missing Setup.hs, version 0.2 (uploaded to Hackage).

--- bin/pickle.hs
+++ bin/pickle.hs
@@ -1,18 +1,69 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main (main) where
 
+import Control.Monad (when)
+import Data.Version (showVersion)
 import qualified Data.ByteString as S
-import System.Environment (getArgs)
+import System.Console.CmdArgs.Implicit
+
+import Paths_python_pickle (version)
 
 import Language.Python.Pickle
 
 main :: IO ()
-main = do
-  [filename] <- getArgs
-  content <- S.readFile filename
-  putStrLn $ "File content:     " ++ show content
-  let value = unpickle content
-  case value of
-    Left err -> putStrLn $ "Unpickling error: " ++ err
-    Right v -> do
-      putStrLn $ "After unpickling: " ++ show v
-      putStrLn $ "After pickling:   " ++ show (pickle v)
+main = (runCmd =<<) . cmdArgs $ modes
+  [ cmdRead
+  ]
+  &= summary versionString
+  &= program "pickle"
+
+-- | String with the program name, version and copyright.
+versionString :: String
+versionString =
+  "Pickle " ++ showVersion version ++ ". Copyright (c) 2012 Vo Minh Thu."
+
+-- | Data type representing the different command-line subcommands.
+data Cmd =
+    Read
+    { cmdFilename :: FilePath
+    , cmdContent :: Bool
+    , cmdUnpickle :: Bool
+    , cmdRepickle :: Bool
+    }
+    -- ^ Parse a pickle file and display some information.
+  deriving (Data, Typeable)
+
+-- | Create a 'Read' command.
+cmdRead :: Cmd
+cmdRead = Read
+  { cmdFilename = def
+    &= argPos 0
+    &= typ "FILE"
+  , cmdContent = def
+    &= explicit
+    &= name "content"
+    &= help "Display the file content `as-is`."
+  , cmdUnpickle = def
+    &= explicit
+    &= name "unpickle"
+    &= help "Display the file content as a Python object."
+  , cmdRepickle = def
+    &= explicit
+    &= name "repickle"
+    &= help "Display the unpickled Python object re-pickled."
+  } &= help "Parse a pickle file and display some information."
+    &= explicit
+    &= name "read"
+
+-- | Run a sub-command.
+runCmd :: Cmd -> IO ()
+runCmd Read{..} = do
+  content <- S.readFile cmdFilename
+  when cmdContent $ putStrLn $ "File content:       " ++ show content
+  when (cmdUnpickle || cmdRepickle) $ do
+    case unpickle content of
+      Left err -> putStrLn $ "Unpickling error:   " ++ err
+      Right v -> do
+        when cmdUnpickle $ putStrLn $ "Unpickled object:   " ++ show v
+        when cmdRepickle $ putStrLn $ "Re-pickled content: " ++ show (pickle v)
